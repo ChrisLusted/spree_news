9141f2523bf5e4dc836f1d2645b34d8223f50206
ChrisLusted <kipl20@hotmail.com>
2012-05-29 15:08:29 +0200

    Update app/models/spree/post.rb

--- app/models/spree/post.rb
+++ app/models/spree/post.rb
@@ -5,4 +5,8 @@ class Spree::Post < ActiveRecord::Base
   scope :published, lambda { where(:published => true) }
   
   attr_accessible :title, :description, :published
+  
+  def rendered_desc
+    RDiscount.new(description.to_s).to_html.html_safe
+  end
 end
